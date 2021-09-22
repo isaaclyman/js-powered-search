2f2523f7765aca5b6dd59f382b36e6351956c6e9
Isaac Lyman <isaac.lyman@healthcatalyst.com>
2021-09-22 11:30:49 -0600

    Improve usability of template file

--- src/resources/TEMPLATE.module.ts
+++ src/resources/TEMPLATE.module.ts
@@ -1,23 +1,51 @@
 export const TEMPLATE = `
 
-export interface SearchOptions {
-  includeFilePatterns?: string[]; // globs to include, e.g. ['**/*.ts']. Searches all files by default.
-  excludeFilePatterns?: string[]; // globs to exclude.
-  includeNodeModules?: boolean; // (default: false) true if node_modules should be searched. Strongly discouraged.
-  maxFileSizeInKB?: number; // (default: 1000) any files larger than this will be skipped.
-  onlyTestLinesInMatchingFiles?: boolean; // (default: false) true if searchByLine should only be used on files that pass searchByFile
-  matchTestingTimeoutInSeconds?: number; // (default: 5) search is tested on a single file preliminarily. If your matchers takes longer than this, an error will appear.
-}
+//
+// Edit these functions to define search parameters. Type definitions are at the bottom of this file.
+//
 
 export function getSettings(): SearchOptions {
   return {
-    // includeFilePatterns: [],
-    // excludeFilePatterns: [],
-    // includeNodeModules: false,
-    // maxFileSizeInKB: 1000,
-    // onlyTestLinesInMatchingFiles: false
-    // matchTestingTimeoutInSeconds: 5
+    // includeFilePatterns: [],              // globs to include, e.g. ['**/*.ts']. Searches all files by default.
+    // excludeFilePatterns: [],              // globs to exclude.
+    // includeNodeModules: false,            // (default: false) true if node_modules should be searched. Strongly discouraged.
+    // maxFileSizeInKB: 1000,                // (default: 1000) any files larger than this will be skipped.
+    // onlyTestLinesInMatchingFiles: false,  // (default: false) true if searchByLine should only be used on files that pass searchByFile
+    // matchTestingTimeoutInSeconds: 5       // (default: 5) search is tested on a single file preliminarily. If your matchers takes longer than this, an error will appear.
   };
+}
+
+export function searchByLine(): LineSearchOptions {
+  return {
+    // A function that accepts a line of text and determines whether it matches your search.
+    // If you only want to search by file, set this method to undefined.
+    doesLineMatchSearch: (line: string, metadata: LineSearchMetadata) => {
+      return line.includes("exactly what I'm looking for");
+    },
+  };
+}
+
+export function searchByFile(): FileSearchOptions {
+  return {
+    // A function that accepts a file (as a text string) and determines whether the file matches your search.
+    // If you only want to search by line, set this method to undefined.
+    doesFileMatchSearch: (file: string, metadata: FileSearchMetadata) => {
+      return file.includes("another thing I'm looking for");
+    },
+  };
+}
+
+//
+// Don't edit these interfaces; they're built into JSPS.
+//
+
+export interface SearchOptions {
+  includeFilePatterns?: string[];
+  excludeFilePatterns?: string[];
+  includeNodeModules?: boolean;
+  maxFileSizeInKB?: number;
+  onlyTestLinesInMatchingFiles?: boolean;
+  matchTestingTimeoutInSeconds?: number;
 }
 
 export interface LineSearchMetadata {
@@ -26,17 +54,7 @@ export interface LineSearchMetadata {
 }
 
 export interface LineSearchOptions {
-  // A function that accepts a line of text and determines whether it matches your search.
-  // If you only want to search by file, set this method to undefined.
   doesLineMatchSearch?: (line: string, metadata: LineSearchMetadata) => boolean;
-}
-
-export function searchByLine(): LineSearchOptions {
-  return {
-    doesLineMatchSearch: (line, metadata) => {
-      return line.includes("exactly what I'm looking for");
-    },
-  };
 }
 
 export interface FileSearchMetadata {
@@ -46,20 +64,10 @@ export interface FileSearchMetadata {
 }
 
 export interface FileSearchOptions {
-  // A function that accepts a file (as a text string) and determines whether the file matches your search.
-  // If you only want to search by line, set this method to undefined.
   doesFileMatchSearch?: (
     fileContents: string,
     metadata: FileSearchMetadata
   ) => boolean;
-}
-
-export function searchByFile(): FileSearchOptions {
-  return {
-    doesFileMatchSearch: (file, metadata) => {
-      return file.includes("another thing I'm looking for");
-    },
-  };
 }
 
 `.trim();
